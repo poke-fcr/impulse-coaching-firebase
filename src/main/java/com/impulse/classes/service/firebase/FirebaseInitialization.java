095950207f1601f0f1a14a9bab38062a83fd57d0
vishal <poke.fcr@gmail.com>
2023-05-07 12:23:04 +0530

    file path changed

--- src/main/java/com/impulse/classes/service/firebase/FirebaseInitialization.java
+++ src/main/java/com/impulse/classes/service/firebase/FirebaseInitialization.java
@@ -1,9 +1,15 @@
 package com.impulse.classes.service.firebase;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -17,15 +23,16 @@ public class FirebaseInitialization {
 	@PostConstruct
 	public void initializaFirebase() {
 
-		FileInputStream serviceAccount;
 		try {
-			serviceAccount = new FileInputStream("serviceAccountKey.json");
-
+			File file = ResourceUtils.getFile("classpath:serviceAccountKey.json");
+//			FileInputStream serviceAccount = new FileInputStream("/serviceAccountKey.json");
+			FileInputStream serviceAccount = new FileInputStream(file);
 			FirebaseOptions options = new FirebaseOptions.Builder()
 					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
 					.setDatabaseUrl("https://mysql-6ebe8-default-rtdb.asia-southeast1.firebasedatabase.app").build();
 
 			FirebaseApp.initializeApp(options);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
